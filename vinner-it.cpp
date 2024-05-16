1c2ea0bd6ca311d83253a6a3eb027924f945fb48
Mohsen Emami <mohsen.emami@scania.com>
2024-05-16 14:13:32 +0200

    Create fresh project

---
+++ vinner-it.cpp
@@ -0,0 +1,12 @@
+﻿// vinner-it.cpp : Defines the entry point for the application.
+//
+
+#include "vinner-it.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
