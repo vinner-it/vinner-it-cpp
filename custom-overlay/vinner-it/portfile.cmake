bae9befe08b1330020a0491fd2737d88744373ba
Mohsen Emami <mohsen.emami@scania.com>
2024-05-16 13:48:34 +0200

    Move CMakeLists one level up

--- custom-overlay/vinner-it/portfile.cmake
+++ custom-overlay/vinner-it/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vinner-it/vinner-it-cpp
     REF "${VERSION}"
-    SHA512 0  # This is a temporary value. We will modify this value in the next section.
+    SHA512 f78b12d7bcb08388c4cc40aa39f84325ecfaae8b596738db2223c7942d953bd63df20e606d288d93aff40ced821249f383e4171d535e2053cbf8586fbd9a4dc4  # This is a temporary value. We will modify this value in the next section.
     HEAD_REF main
 )
 
