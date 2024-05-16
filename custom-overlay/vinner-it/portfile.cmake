9936b8df003ca9625f300d6c1bef73f4ae04981a
Mohsen Emami <mohsen.emami@scania.com>
2024-05-16 14:01:46 +0200

    Fix CMakeLists executable

--- custom-overlay/vinner-it/portfile.cmake
+++ custom-overlay/vinner-it/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vinner-it/vinner-it-cpp
     REF "${VERSION}"
-    SHA512 f78b12d7bcb08388c4cc40aa39f84325ecfaae8b596738db2223c7942d953bd63df20e606d288d93aff40ced821249f383e4171d535e2053cbf8586fbd9a4dc4  # This is a temporary value. We will modify this value in the next section.
+    SHA512 0b149bd8f8870f104387e5ce32fe02a5c55d7e4e5ae42c5d5fe5053b22742fe8919a838a69bc1cb95f5668aa999bd57a4d1675b054d1576ba2df838c02b1e69e  # This is a temporary value. We will modify this value in the next section.
     HEAD_REF main
 )
 
